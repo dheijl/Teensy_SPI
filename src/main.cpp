e761245776122e09e2621cd80223363acae03454
dheijl <danny.heijl@telenet.be>
2024-12-22 17:28:36 +0100

    il9341 setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,119 @@
 #include <Arduino.h>
 
 // https://github.com/vindar/ILI9341_T4
+// https://github.com/vindar/ILI9341_T4/blob/main/examples/dualDisplay/dualDisplay.ino
 // https://vindar.github.io/tgx/html/md_docs_intro_api_2_d.html#subsec_text
 
 #include <SPI.h>
 #include <ILI9341_T4.h>
-#include "font_tgx_Arial.h"
-#include "font_tgx_OpenSans.h"
 
-static tgx::RGB32 buffer[320 * 240];                //
-static tgx::Image<tgx::RGB32> im(buffer, 320, 240); // create a black 320x240 image.
+// the screen driver library
+#include <ILI9341_T4.h>
+
+//
+// FIRST SCREEN IS WIRED TO SPI0
+//
+/*
+#define PIN_SCK0 13  // mandatory
+#define PIN_MISO0 12 // mandatory  (if the display has no MISO line, set this to 255 but then VSync will be disabled)
+#define PIN_MOSI0 11 // mandatory
+#define PIN_DC0 10   // mandatory, can be any pin but using pin 10 (or 36 or 37 on T4.1) provides greater performance
+
+#define PIN_CS0 9          // optional (but recommended), can be any pin.
+#define PIN_RESET0 6       // optional (but recommended), can be any pin.
+#define PIN_BACKLIGHT0 255 // optional, set this only if the screen LED pin is connected directly to the Teensy.
+#define PIN_TOUCH_IRQ0 255 // optional, set this only if the touchscreen is connected on the same SPI bus
+#define PIN_TOUCH_CS0 255  // optional, set this only if the touchscreen is connected on the same SPI bus
+*/
+//
+// SECOND SCREEN IS WIRED TO SPI1
+//
+#define PIN_SCK1 27  // mandatory
+#define PIN_MISO1 1  // mandatory  (if the display has no MISO line, set this to 255 but then VSync will be disabled)
+#define PIN_MOSI1 26 // mandatory
+#define PIN_DC1 0    // mandatory, can be any pin but using pin 0 (or 38 on T4.1) provides greater performance
+
+#define PIN_CS1 30         // optional (but recommended), can be any pin.
+#define PIN_RESET1 29      // optional (but recommended), can be any pin.
+#define PIN_BACKLIGHT1 255 // optional, set this only if the screen LED pin is connected directly to the Teensy.
+#define PIN_TOUCH_IRQ1 255 // optional. set this only if the touchscreen is connected on the same SPI bus
+#define PIN_TOUCH_CS1 255  // optional. set this only if the touchscreen is connected on the same SPI bus
+
+#define SPI_SPEED 240000000
+
+// screen size in portrait mode
+#define LX 240
+#define LY 320
+
+// screen driver objects
+// ILI9341_T4::ILI9341Driver tft0(PIN_CS0, PIN_DC0, PIN_SCK0, PIN_MOSI0, PIN_MISO0, PIN_RESET0, PIN_TOUCH_CS0, PIN_TOUCH_IRQ0); // for screen on SPI0
+ILI9341_T4::ILI9341Driver tft1(PIN_CS1, PIN_DC1, PIN_SCK1, PIN_MOSI1, PIN_MISO1, PIN_RESET1, PIN_TOUCH_CS1, PIN_TOUCH_IRQ1); // for screen on SPI1
+
+// framebuffers
+//  uint16_t internal_fb0[LX * LY]; // internal fb used by the library for the first screen
+DMAMEM uint16_t internal_fb1[LX * LY]; // internal fb used by the library for the second screen
+uint16_t fb[LX * LY];                  // main framebuffer we draw onto
+
+// 4 diff buffers with about 6K memory each
+/*
+ILI9341_T4::DiffBuffStatic<6000> diff1; // for the driver on SPI0
+ILI9341_T4::DiffBuffStatic<6000> diff2; //
+*/
+ILI9341_T4::DiffBuffStatic<6000> diff3; // for the driver on SPI1
+ILI9341_T4::DiffBuffStatic<6000> diff4; //
 
 void setup()
 {
   // put your setup code here, to run once:
-  im.clear(tgx::RGB32_Black);
+  Serial.begin(9600);
 
-  // drawing text: the anchor point use the baseline.
-  im.drawText("non-AA font are ugly...", {5, 10}, font_tgx_Arial_8, tgx::RGB32_Blue); // a non AA font
-  im.drawText("AA font, 9pt", {5, 25}, font_tgx_OpenSans_9, tgx::RGB32_Green);        // AA font, 9pt
-  im.drawText("AA font, 10pt", {5, 40}, font_tgx_OpenSans_10, tgx::RGB32_Green);      // AA font, 10pt
-  im.drawText("AA font, 14pt", {5, 60}, font_tgx_OpenSans_14, tgx::RGB32_Green);      // AA font, 14pt
-  im.drawText("AA font, 18pt", {5, 83}, font_tgx_OpenSans_20, tgx::RGB32_Green);      // AA font, 18pt
+  // use USB serial for debug information / troubleshooting.
+  // tft0.output(&Serial);
+  tft1.output(&Serial);
 
-  // drawing on the baseline
-  im.drawFastHLine({130, 30}, 160, tgx::RGB32_White, 0.5f);
-  im.drawFastHLine({130, 45}, 160, tgx::RGB32_White, 0.5f);
-  im.drawText("The quick brown fox jumps", {130, 30}, font_tgx_OpenSans_12, tgx::RGB32_Orange);
-  im.drawText("over the lazy dog !", {130, 45}, font_tgx_OpenSans_12, tgx::RGB32_Orange);
+  // init the screens
+  /*
+  while (!tft0.begin(SPI_SPEED))
+  {
+    Serial.print("\n\n\n ****** ERROR CONNECTING TO ILI9341 SCREEN ON SPI 0 ****** \n\n");
+    delay(1000);
+  }
+  */
+  while (!tft1.begin(SPI_SPEED))
+  {
+    Serial.print("\n\n\n ****** ERROR CONNECTING TO ILI9341 SCREEN ON SPI 1 ****** \n\n");
+    delay(1000);
+  }
+  /*
+    // set parameters for the first display
+    tft0.setRotation(0);                 // portrait mode 240 x320
+    tft0.setFramebuffer(internal_fb0);   // set the internal framebuffer (enables double buffering)
+    tft0.setDiffBuffers(&diff1, &diff2); // set the 2 diff buffers => activate differential updates.
+    tft0.setDiffGap(4);                  // use a small gap for the diff buffers
+    tft0.setRefreshRate(150);            // max refresh rate.
+    tft0.setVSyncSpacing(2);             // set framerate = refreshrate/2 (and enable vsync at the same time).
+  */
+  // set parameters for the second display
+  tft1.setRotation(0);                 // portrait mode 240 x320
+  tft1.setFramebuffer(internal_fb1);   // set the internal framebuffer (enables double buffering)
+  tft1.setDiffBuffers(&diff3, &diff4); // set the 2 diff buffers => activate differential updates.
+  tft1.setDiffGap(4);                  // use a small gap for the diff buffers
+  tft1.setRefreshRate(120);            // max refresh rate.
+  tft1.setVSyncSpacing(2);             // set framerate = refreshrate/2 (and enable vsync at the same time).
 
-  // finding the bounding box of a text.
-  const char *tbb = "Text bounding box";
-  tgx::iBox2 bb = im.measureText(tbb, {230, 70}, font_tgx_OpenSans_14, tgx::Anchor::CENTER, false, false);
-  im.fillRect(bb, tgx::RGB32_Yellow, 0.5f);
-  im.drawTextEx(tbb, {230, 70}, font_tgx_OpenSans_14, tgx::Anchor::CENTER, false, false, tgx::RGB32_Blue);
-
-  // draw text with different anchor points using drawTextEx()
-  // -> the anchor is relative to the text bounding box
-  im.drawRect({20, 300, 100, 230}, tgx::RGB32_Gray);
-  im.drawFastHLine({20, 165}, 280, tgx::RGB32_Gray);
-  im.drawFastVLine({160, 100}, 130, tgx::RGB32_Gray);
-  tgx::iVec2 anchor_point;
-
-  anchor_point = {20, 100};
-  im.drawTextEx("TOPLEFT", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::TOPLEFT, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
-
-  anchor_point = {300, 100};
-  im.drawTextEx("TOPRIGHT", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::TOPRIGHT, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
-
-  anchor_point = {20, 230};
-  im.drawTextEx("BOTTOMLEFT", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::BOTTOMLEFT, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
-
-  anchor_point = {300, 230};
-  im.drawTextEx("BOTTOMRIGHT", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::BOTTOMRIGHT, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
-
-  anchor_point = {160, 100};
-  im.drawTextEx("CENTERTOP", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::CENTERTOP, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
-
-  anchor_point = {160, 230};
-  im.drawTextEx("CENTERBOTTOM", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::CENTERBOTTOM, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
-
-  anchor_point = {20, 165};
-  im.drawTextEx("CENTERLEFT", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::CENTERLEFT, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
-
-  anchor_point = {300, 165};
-  im.drawTextEx("CENTERRIGHT", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::CENTERRIGHT, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
-
-  anchor_point = {160, 165};
-  im.drawTextEx("CENTER", anchor_point, font_tgx_OpenSans_10, tgx::Anchor::CENTER, true, true, tgx::RGB32_Red);
-  im.fillCircleAA(anchor_point, 1.5, tgx::RGB32_Green);
+  // make sure backlight is on
+  /*
+  if (PIN_BACKLIGHT0 != 255)
+  {
+    pinMode(PIN_BACKLIGHT0, OUTPUT);
+    digitalWrite(PIN_BACKLIGHT0, HIGH);
+  }
+  */
+  if (PIN_BACKLIGHT1 != 255)
+  {
+    pinMode(PIN_BACKLIGHT1, OUTPUT);
+    digitalWrite(PIN_BACKLIGHT1, HIGH);
+  }
 }
 
 void loop()

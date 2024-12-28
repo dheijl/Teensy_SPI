067cb313b94c0d27d20fb66e98d659310dcfa2d6
dheijl <danny.heijl@telenet.be>
2024-12-28 20:54:51 +0100

    ili9341_t3n works, ili9341_t3  does NOT

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,37 @@
 
 #include <SPI.h>
 // the screen driver library
-#include <ILI9341_t3.h>
+#include <ILI9341_t3n.h>
 
 //
-// AU IS WIRED TO SPI0
+// IL9341 is WIRED TO SPI1
 //
 const uint8_t PIN_SCK0 = 13;
 const uint8_t PIN_MISO0 = 12;
 const uint8_t PIN_MOSI0 = 11;
 const uint8_t PIN_CS0 = 10;
-/* if SPI0 is IL9341
-#define PIN_DC0 36   // mandatory, can be any pin but using pin 10 (or 36 or 37 on T4.1) provides greater performance
-#define PIN_RESET0 6       // optional (but recommended), can be any pin.
-#define PIN_BACKLIGHT0 255 // optional, set this only if the screen LED pin is connected directly to the Teensy.
-#define PIN_TOUCH_IRQ0 255 // optional, set this only if the touchscreen is connected on the same SPI bus
-#define PIN_TOUCH_CS0 255  // optional, set this only if the touchscreen is connected on the same SPI bus
-*/
+// if SPI0 is IL9341
+const uint8_t PIN_DC0 = 36;         // mandatory, can be any pin but using pin 10 (or 36 or 37 on T4.1) provides greater performance
+const uint8_t PIN_RESET0 = 6;       // optional (but recommended), can be any pin.
+const uint8_t PIN_BACKLIGHT0 = 255; // optional, set this only if the screen LED pin is connected directly to the Teensy.
+const uint8_t PIN_TOUCH_IRQ0 = 255; // optional, set this only if the touchscreen is connected on the same SPI bus
+const uint8_t PIN_TOUCH_CS0 = 255;  // optional, set this only if the touchscreen is connected on the same SPI bus
+
 //
-// IL9341 is WIRED TO SPI1
+// AU IS WIRED TO SPI1
 //
 const uint8_t PIN_SCK1 = 27;
 const uint8_t PIN_MISO1 = 1;  // mandatory  (if the display has no MISO line, set this to 255 but then VSync will be disabled)
 const uint8_t PIN_MOSI1 = 26; // mandatory
 const uint8_t PIN_DC1 = 38;   // mandatory, can be any pin but using pin 0 (or 38 on T4.1) provides greater performance
 
-const uint8_t PIN_CS1 = 0;         // optional (but recommended), can be any pin.
-const uint8_t PIN_RESET1 = 29;      // optional (but recommended), can be any pin.
+const uint8_t PIN_CS1 = 0;          // optional (but recommended), can be any pin.
+const uint8_t PIN_RESET1 = 255;     // 29 optional (but recommended), can be any pin.
 const uint8_t PIN_BACKLIGHT1 = 255; // optional, set this only if the screen LED pin is connected directly to the Teensy.
 const uint8_t PIN_TOUCH_IRQ1 = 255; // optional. set this only if the touchscreen is connected on the same SPI bus
 const uint8_t PIN_TOUCH_CS1 = 255;  // optional. set this only if the touchscreen is connected on the same SPI bus
 
-const unsigned long SPI_SPEED = 24000000;
+const unsigned long SPI_SPEED = 4000000;
 
 // screen size in portrait mode
 #define LPORTRAIT 1
@@ -49,12 +49,12 @@ const unsigned long SPI_SPEED = 24000000;
 #define LYP 240
 */
 // screen driver object
-ILI9341_t3 tft(PIN_CS1, PIN_DC1, PIN_RESET1, PIN_MOSI1, PIN_SCK1, PIN_MISO1); // for screen on SPI1
+ILI9341_t3n tft(PIN_CS0, PIN_DC0, PIN_RESET0, PIN_MOSI0, PIN_SCK0, PIN_MISO0); // for screen on SPI1
 
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   while (!Serial)
     ; // wait for Arduino Serial Monitor
   Serial.println("ILI9341 Test!");
@@ -66,17 +66,36 @@ void setup()
     // digitalWrite(PIN_BACKLIGHT1, HIGH);
     analogWrite(PIN_BACKLIGHT1, 128); // PWM backlight level 50 %
   }
-  tft.setClock(SPI_SPEED);
+  /*
+  pinMode(PIN_CS1, OUTPUT);
+  SPI.begin();
+  SPI1.setCS(PIN_CS1);
+  SPI1.setMOSI(PIN_MOSI1);
+  SPI1.setMISO(PIN_MISO1);
+  SPI1.setSCK(PIN_SCK1);
+  SPI1.begin();
+  SPI1.beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
+  SPI1.transfer16(0x1234);
+  SPI1.endTransaction();
+*/
+  pinMode(PIN_CS0, OUTPUT);
+  tft.begin();
+  // tft.setClock(SPI_SPEED);
   tft.fillScreen(ILI9341_BLACK);
+  /**/
   tft.setTextColor(ILI9341_YELLOW);
   tft.setRotation(LPORTRAIT);
   tft.setTextSize(2);
   tft.println("Waiting for Arduino Serial Monitor...");
 
-  tft.println();
+  // tft.println();
+  /**/
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  Serial.println("Tick");
+  tft.println("Tick");
+  delay(1000);
 }
